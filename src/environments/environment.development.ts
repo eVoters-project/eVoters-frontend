8bea3af9bfd90ed4c6c13d8af9302d4e51c8bb6d
ceb1rparba <ceb1rparba@gmail.com>
2024-07-14 05:04:16 +0800

    update env file switch in prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // urlAPI: 'http://143.198.91.135:5005/api/v1'
-  urlAPI: 'http://localhost:3000/api/v1'
+  urlAPI: 'http://143.198.91.135:5005/api/v1'
 };
