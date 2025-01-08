f142e9e84f03d0251ffdfad28faef4fdf0e205bf
John Stephen Entia <juanmerbbss08@gmail.com>
2025-01-08 10:42:46 +0800

    Update environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // urlAPI: 'http://localhost:3000/api/v1',
-  urlAPI: 'http://146.190.136.183:2004/docs/swagger'
+  urlAPI: 'http://146.190.136.183:2004/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // urlAPI: 'http://143.198.91.135:5005/api/v1'
-  urlAPI: 'http://146.190.136.183:2004/docs/swagger'
+  urlAPI: 'http://146.190.136.183:2004/api/v1'
 };
